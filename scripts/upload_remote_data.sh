29d8b0407de9b423363209dca4c5a0df8ebb517b
Manuel Kaufmann <humitos@gmail.com>
2011-03-04 18:05:53 -0300

    remote data upload updated

--- scripts/upload_remote_data.sh
+++ scripts/upload_remote_data.sh
@@ -1,12 +1,13 @@
 ./download_csv_files.sh
 
-../google_appengine/appcfg.py upload_data \
+../../google_appengine/appcfg.py upload_data \
     --email=humitos@gmail.com \
     --config_file=../loaders/$1_loader.py \
     --filename=$1_data.csv \
     --kind=$2 ../ \
     --has_header \
     --log_file=../../bulkloader/`date +%s`.log \
-    --db_filename=../../bulkloader/`date +%s`.sql3
+    --db_filename=../../bulkloader/`date +%s`.sql3 \
+    --batch_size=100
 
-./remove_csv_files.sh
+# ./remove_csv_files.sh
