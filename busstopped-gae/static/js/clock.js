7719588947054cd14234b6e5d254d8536535959f
Manuel Kaufmann <humitos@gmail.com>
2011-03-20 18:11:46 -0300

    show full date in Menu bar

--- busstopped-gae/static/js/clock.js
+++ busstopped-gae/static/js/clock.js
@@ -9,16 +9,28 @@ function clock_tick(){
     minutes = now.getMinutes();
     seconds = now.getSeconds();
 
+    day = now.getDate();
+    month = now.getMonth();
+
     if (hours <= 9)
         hours = "0" + hours;
     if (minutes <= 9)
         minutes = "0" + minutes;
     if (seconds <= 9)
         seconds = "0" + seconds;
+    if (month <= 9)
+	month = "0" + month;
+    if (day <= 9)
+	day = "0" + day;
 
     liveclock = $("#liveclock");
+    liveclock.html('');
+    weekday = '<div style="text-align: center;font-size: 17px">' + WEEKDAY + '</div>';
+    liveclock.append(weekday);
+    date = '<div style="text-align: center;font-size: 17px">' + day + '/' + month + '/' + now.getFullYear() + '</div>';
+    liveclock.append(date);
     content = "<b>" + hours + ":" + minutes + ":" + seconds + "</b>";
-    liveclock.html(content);
+    liveclock.append(content);
     setTimeout("clock_tick()", 1000);
 }
 
